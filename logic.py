dfc4d1ebc3740515b060d9e5fde5b88208fcc379
linda27huang <huangyuqi1999@gmail.com>
2022-11-07 00:34:14 -0800

    1107code

--- logic.py
+++ logic.py
@@ -8,7 +8,36 @@ def make_empty_board():
     ]
 
 def get_winner(board):
-    return None # FIXME
+    for x in range(3):
+        if board[x][0] == board[x][1] == board[x][2] == 'x':  # check rows x
+            return 'x'
+        elif board[x][0] == board[x][1] == board[x][2] == 'o': # check rows o
+            return 'o'
+        elif board[0][x] == board[1][x] == board[2][x] == 'x':  # check cols x
+            return 'x'
+        elif board[0][x] == board[1][x] == board[2][x] == 'o':  # check cols o
+            return 'o'
+    if board[0][0] == board[1][1] == board[2][2] == 'x':    # check diagonals x 
+        return 'x'
+    elif board[0][2] == board[1][1] == board[2][0] == 'x':
+        return 'x'
+    if board[0][0] == board[1][1] == board[2][2] == 'o':    # check diagonals o
+        return 'o'
+    elif board[0][2] == board[1][1] == board[2][0] == 'o':
+        return 'o'
+    else:
+        return None 
+
+def announce_winner(winner):
+    res = ""
+    if winner == 'x':
+        res = 'X win!'
+    elif winner == 'o':
+        res = 'O win!'
+    else:
+        res = 'draw'
+    print(res)
+    return res
 
 def other_player(player):
     return '0' # FIXME
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-# This is the file contains the Command 
+# This is the file contains the Command Line Interface(CLI) for the game
 import unittest
 import logic
 
@@ -6,12 +6,26 @@ class TestLogic(unittest.TestCase):
 
     def test_get_winner(self):
         board = [
-            ['X', None, '0'],
-            [None, 'X', None],
-            [None, '0', 'X']
+            ['x', None, 'o'],
+            ['x', None, None],
+            ['x', 'o', 'x']
         ]
-        self.assertEqual(logic.get_winner(board),'X')
+        
+        self.assertEqual(logic.get_winner(board),'x')
 
+    def test_make_empty_board(self):
+        board = [
+        [None, None, None],
+        [None, None, None],
+        [None, None, None]
+        ]
+        self.assertEqual(logic.make_empty_board(),board)     
+
+    def test_announce_winner(self):
+        winner = 'x'
+        winner = 'o'
+        self.assertEqual(logic.announce_winner(winner),'O win!')  
+        #self.assertEqual(logic.announce_winner(winner),print('O win!'))          
     # TODO: test all functions from logic.py
 
 if __name__ == '__main__':
