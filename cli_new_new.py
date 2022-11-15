dfbc22f6dece2cdf9bdf1b971af9e52fba94cf50
linda27huang <huangyuqi1999@gmail.com>
2022-11-15 15:09:24 -0800

    1115code

---
+++ cli_new_new.py
@@ -0,0 +1,18 @@
+from logic_new_new import Board
+#from logic_new_new import Game
+#from logic_new_new import Human
+#from logic_new_new import Bot
+#from logic_new_new import 
+
+if __name__ == '__main__':
+    game = Board()
+    
+    print('lets play tictactoe!')
+    player = int(input('enter 1 human_human play, enter 2 human_computer play'))
+    if player == 1:
+        game.play_human_human()
+    elif player == 2:
+        game.play_human_computer()
+
+
+
