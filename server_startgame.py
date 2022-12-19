33c81eb7590a6271691fbd2c38de6576c7e04572
linda27huang <huangyuqi1999@gmail.com>
2022-12-18 23:40:20 -0800

    final tictactoe

--- server_startgame.py
+++ server_startgame.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from logic_1218 import Game
+import time
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -7,5 +11,18 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/play")
+def play():
+    print("Initializing game...")
+    global game, last_time
+    game = Game
+    last_time = time.time()
+    if request.args.get('playmode') == 'humanhuman':
+        game = Game(game_mode=request.args.get('playmode'), start_first=True, player_names=[request.args.get('player1'), request.args.get('player2')])
+        return render_template('play.html', player1=request.args.get('player1'), player2=request.args.get('player2'))
+    elif request.args.get('playmode') == 'humanbot':
+        game = Game(game_mode=request.args.get('playmode'), bot_type=request.args.get('botmode2'), start_first=True, player_names=[request.args.get('player1')])
+        return render_template('play.html', player1=request.args.get('player1'), player2=request.args.get('botmode2') + 'Bot')
+    
 if __name__ == '__main__':
     app.run(debug=True)
