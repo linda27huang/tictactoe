5de0be682a0e135f0907df737068461ade88985c
linda27huang <huangyuqi1999@gmail.com>
2022-12-18 18:20:52 -0800

    final tictactoe

--- cli_1218.py
+++ cli_1218.py
@@ -1,11 +1,15 @@
-from logic_new import Game
-#from logic_new_new import Game
-#from logic_new_new import Human
-#from logic_new_new import Bot
+from logic_1218 import Game
+from logic_1218 import Board
+from logic_1218 import Human
+from logic_1218 import Bot
 #from logic_new_new import 
 
 if __name__ == '__main__':
-    game = Game()
+
+    human = Human()
+    bot = Bot()
+    board = Board()
+    game = Game(human,bot,board)
     
     print('lets play tictactoe!')
     player = int(input('enter 1 human_human play, enter 2 human_computer play'))
